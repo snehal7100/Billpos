16f0d1ec4f40cc0123c522650df2f8c5ec4066f2
= <=>
2024-12-04 09:42:00 +0530

    kk

---
+++ category/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    c_name=models.CharField(max_length=255)
+    c_img=models.FileField(upload_to="category/",max_length=255,null=True)
+    B_img=models.FileField(upload_to="category/",max_length=255,null=True)
+    
+    class Meta:
+        db_table = 'tbl_category'
