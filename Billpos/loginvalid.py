ffcf20ddb1477c92e7f16da4d8794fa87172687f
= <=>
2024-12-05 09:54:28 +0530

    modify

--- Billpos/loginvalid.py
+++ Billpos/loginvalid.py
@@ -3,23 +3,16 @@ from django.shortcuts import render, redirect
 
 def login_view(request):
     if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        # Check if fields are filled
-        if not email or not password:
+        username = request.POST.get('uname')
+        password = request.POST.get('passw')
+        if not username or not password:
             messages.error(request, "Both email and password are required.")
             return render(request, 'Component/index.html')
-
-        # Explicitly check if the email and password match the required values
-        if email == "sukhdi@gmail.com" and password == "sukhda@":
-            # Successful login, redirect to the admin dashboard
+        if username == "admin@gmail.com" and password == "admin":
             messages.success(request, f"Welcome back, Admin!")
-            return redirect('/admin/')  # Redirect to Django admin or your admin dashboard URL
+            return redirect('/index/')  
         else:
-            # Invalid credentials, show error message and render login form again
             messages.error(request, "Invalid email or password.")
             return render(request, 'Component/index.html')
-
-  
+        
     return render(request, 'Component/index.html')
