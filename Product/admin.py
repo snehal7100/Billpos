9fa7673e7b76d3e91560b973b20db6a4a86133a7
snehal17100 <snehalpawar212004@gmail.com>
2024-12-13 22:39:54 +0530

    modify

--- Product/admin.py
+++ Product/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from Product.models import ProductModel
 
 class productAdmin(admin.ModelAdmin):
-    list_display=("pname","hsncode","category","brand","tax","punit","aunit",)
+    list_display=("pname","hsncode","category","brand","tax","taxtype","punit","aunit","cfactor","uprice",)
 admin.site.register(ProductModel,productAdmin)
