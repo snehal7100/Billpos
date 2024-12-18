8a66d301aa29e0fe35ac707bf2d6a45448b989cf
= <=>
2024-12-18 11:16:52 +0530

    change

--- Product/models.py
+++ Product/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class ProductModel(models.Model):
-<<<<<<< HEAD
 
     pname = models.CharField(max_length=255)
     hsncode = models.CharField(max_length=255)
@@ -19,21 +18,3 @@ class ProductModel(models.Model):
 
     def __str__(self):
         return self.pname
-
-=======
->>>>>>> 402f905256171b4ed8e09e5de2a30224d8b18b23
-    pid=models.CharField(max_length=255)
-    pname=models.CharField(max_length=255)
-    hsncode=models.CharField(max_length=255)
-    category=models.CharField(max_length=255)
-    brand=models.CharField(max_length=255)
-    tax=models.CharField(max_length=255)
-    taxtype=models.CharField(max_length=255)
-    punit=models.CharField(max_length=255)
-    aunit=models.CharField(max_length=255)
-    cfactor = models.CharField(max_length=255, default='1')
-    price=models.CharField(max_length=255, default='1')
-
-    class Meta:
-        db_table = 'tbl_product'
-
