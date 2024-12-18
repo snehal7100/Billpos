f03fb7e67032019d0f55dfaabaa1f64cde8e8cb0
= <=>
2024-12-18 12:23:10 +0530

    lastchange

--- Taxes/models.py
+++ Taxes/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Taxs(models.Model):
-    sr_no =  models.CharField(max_length=255) 
-    taxname = models.CharField(max_length=255)  
-    taxpercentage =  models.CharField(max_length=255)  
-   
+    tid = models.CharField(max_length=255)  # Explicit primary key field
+    sr_no = models.CharField(max_length=255)
+    taxname = models.CharField(max_length=255)
+    taxpercentage = models.CharField(max_length=255)
+
     class Meta:
-        db_table = 'tbl_taxes'  
+        db_table = 'tbl_taxes'
