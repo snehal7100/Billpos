aebca89c83d0df7ae0304c433871dbb13b19dff7
snehal17100 <snehalpawar212004@gmail.com>
2024-12-18 11:31:15 +0530

    modify

--- Product/models.py
+++ Product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ProductModel(models.Model):
-
+    pid= models.CharField(max_length=255)
     pname = models.CharField(max_length=255)
     hsncode = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
@@ -11,10 +11,9 @@ class ProductModel(models.Model):
     punit = models.CharField(max_length=255)
     aunit = models.CharField(max_length=255)
     cfactor = models.DecimalField(max_digits=10, decimal_places=2, default=1.0)
-    uprice = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         db_table = 'tbl_product'
 
-    def __str__(self):
-        return self.pname
+   
