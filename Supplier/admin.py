c6d26a23922f3569a1622685e207cbfe30e8c861
= <=>
2024-12-23 20:54:31 +0530

    change

--- Supplier/admin.py
+++ Supplier/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from Supplier.models import Suppliers
 
-# Register your models here.
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", 
+        "mobile_no", 
+        "email", 
+        "gst_no", 
+        "pan_no", 
+        "opening_balance", 
+        "group_type", 
+        "billing_address", 
+        "shipping_address", 
+        "city", 
+        "credit_limit", 
+        "credit_period", 
+        "supplier_barcode",
+    )
+
+admin.site.register(Suppliers, SupplierAdmin)
