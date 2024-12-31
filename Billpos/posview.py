dfd8d1825511d9e9cf94e5a6b4f4141e9746c651
= <=>
2024-12-31 10:22:57 +0530

    change

--- Billpos/posview.py
+++ Billpos/posview.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from Customer.models import Customer
+from Product.models import ProductModel
 
 def posdashboard(request):
-    # Fetch customer data
-    cData = Customer.objects.all()
-
-    return render(request, "Pos/posindex.html",{"customerData": cData})  # Render the same template
-
-
+    # Fetch customer and product data
+    cData = Customer.objects.all()  # Fetch all customer records
+    pData = ProductModel.objects.all()  # Fetch all product records
+    return render(request, "Pos/posindex.html", {"customerData": cData, "productData": pData})
