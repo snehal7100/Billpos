2976d16bb0489a44090b67202d5376f0608caab0
snehal17100 <snehalpawar212004@gmail.com>
2025-01-13 11:41:42 +0530

    modify

--- Billpos/barcodeview.py
+++ Billpos/barcodeview.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from Product.models import ProductModel
+from category.models import Category 
 
 def Barcodes(request):
+    category_query = "SELECT * FROM tbl_category"
+    categoryData = Category.objects.raw(category_query)
     pData = ProductModel.objects.all()  
-    return render(request, "Barcode/index.html", {"productData": pData})
+    context = {
+        "productData": pData,
+        "categoryData": categoryData
+    }
+    return render(request, "Barcode/index.html", context)
